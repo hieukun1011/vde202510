90a181adfd98763ad47b58d22fc9f5fdf93dc982
hieukun1011 <hieukun1011@gmail.com>
2025-12-18 15:38:19 +0700

    update

--- build_hieudt/odoo-aws/01-create-vpc.sh
+++ build_hieudt/odoo-aws/01-create-vpc.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+set -e
+
 source ./env.sh
 
 echo "🚀 Creating VPC..."
 
 VPC_ID=$(aws ec2 create-vpc \
+  --region $REGION \
   --cidr-block $VPC_CIDR \
   --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=$PROJECT-vpc}]" \
   --query "Vpc.VpcId" \
   --output text)
 
-aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-support
-aws ec2 modify-vpc-attribute --vpc-id $VPC_ID --enable-dns-hostnames
+aws ec2 modify-vpc-attribute \
+  --region $REGION \
+  --vpc-id $VPC_ID \
+  --enable-dns-support "{\"Value\":true}"
 
-echo "VPC_ID=$VPC_ID" >> .vpc_id
+aws ec2 modify-vpc-attribute \
+  --region $REGION \
+  --vpc-id $VPC_ID \
+  --enable-dns-hostnames "{\"Value\":true}"
+
+echo $VPC_ID > .vpc_id
+
 echo "✅ VPC created: $VPC_ID"
